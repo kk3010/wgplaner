68fdd496a484c476c38a508a85e58a37f8c72637
Karyna Kachanovska <kk153@hdm-stuttgart.de>
2021-11-17 21:01:30 +0100

    condition for env variables

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
+if (import.meta.env.DEV) {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
+
 createApp(App).mount('#app')
