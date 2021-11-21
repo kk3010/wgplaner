1bec4c44a7f35f54e22282bef90363ee7260a890
Karyna Kachanovska <kk153@hdm-stuttgart.de>
2021-11-21 17:02:42 +0100

    update login view

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -12,9 +12,30 @@ function handleLogin() {
 </script>
 
 <template>
-  <form class="form-control max-w-xs" @submit.prevent="handleLogin">
-    <input class="mb-4" v-model="user.email" name="email" type="email" />
-    <input v-model="user.password" name="password" type="password" />
-    <button type="submit">Login</button>
-  </form>
+  <div class="form-control max-w-xs">
+    <span class="m-4 ml-0 text-sm font-semibold uppercase label-text">Login</span>
+    <form @submit.prevent="handleLogin">
+      <div class="form-control">
+        <input
+          class="input input-bordered input-accent mb-4"
+          v-model="user.email"
+          name="email"
+          placeholder="email"
+          type="email"
+        />
+      </div>
+      <div class="form-control">
+        <input
+          class="input input-bordered input-accent mb-4"
+          v-model="user.password"
+          name="password"
+          placeholder="password"
+          type="password"
+        />
+      </div>
+      <div class="form-control">
+        <button class="btn btn-square btn-outline p-4" type="submit">Login</button>
+      </div>
+    </form>
+  </div>
 </template>
