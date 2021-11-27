2b1373a04554e6239560db852673b55d69263ca3
Ennen Chris-Robin <ce053@hdm-stuttgart.de>
2021-11-27 15:04:27 +0100

    close drawer on click

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import SideMenu from '@/components/SideMenu.vue'
 import MenuComponent from '@/components/MenuComponent.vue'
+
+const drawerOpen = ref(false)
 </script>
 
 <template>
   <div class="drawer drawer-mobile">
-    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" v-model="drawerOpen" />
     <main class="flex flex-col drawer-content">
       <MenuComponent />
       <router-view></router-view>
     </main>
-    <SideMenu></SideMenu>
+    <SideMenu @click="drawerOpen = false" />
   </div>
 </template>
