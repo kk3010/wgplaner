47750a869a6c567ff18355be7b73bbaf6388de81
Ennen Chris-Robin <ce053@hdm-stuttgart.de>
2021-12-03 13:53:02 +0100

    add basic seeding for users and flats with constant test user

---
+++ backend/src/flat/flat-seeder.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { generateFakeFlat } from '../../test/flat.mock';
+import { FlatService } from './flat.service';
+import type { IUser } from '../interfaces/user.interface';
+
+@Injectable()
+export class FlatSeederService {
+  constructor(private readonly flatService: FlatService) {}
+
+  create(creator: IUser) {
+    const { id, ...flat } = generateFakeFlat();
+    return this.flatService.create(flat, creator);
+  }
+}
---
+++ backend/src/seed.ts
@@ -0,0 +1,33 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { FlatSeederService } from './flat/flat-seeder.service';
+import { UserSeederService } from './user/user-seeder.service';
+import { UserService } from './user/user.service';
+
+async function bootstrap() {
+  const ctx = await NestFactory.createApplicationContext(AppModule);
+  try {
+    const userService = ctx.get(UserService);
+    const flatSeeder = ctx.get(FlatSeederService);
+    const userSeeder = ctx.get(UserSeederService);
+    const logger = new Logger('Seeder');
+    const testUser = await userService.create({
+      email: 'test@test.de',
+      firstName: 'Test',
+      lastName: 'Mc Testface',
+      password: 'abc123456',
+    });
+    const flat = await flatSeeder.create(testUser);
+    const users = await userSeeder.create(flat.id);
+    logger.log(
+      `Test user: \n email: ${testUser.email} password: ${testUser.password}`,
+    );
+  } catch (e) {
+    console.error(e);
+  } finally {
+    ctx.close();
+  }
+}
+
+bootstrap();
