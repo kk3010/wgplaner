545b53e14c33e1481685d8ff94e97da8ab4a7a3e
Ennen Chris-Robin <ce053@hdm-stuttgart.de>
2021-12-22 16:59:02 +0100

    make wallet service test run

--- backend/src/wallet/wallet.service.spec.ts
+++ backend/src/wallet/wallet.service.spec.ts
@@ -1,15 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
 import { WalletService } from './wallet.service';
+import { Wallet } from './entities/wallet.entity';
+import { MockType } from '../../test/mockType';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockWalletRepositoryFactory: () => MockType<Repository<Wallet>> =
+  () => ({});
 
 describe('WalletService', () => {
   let service: WalletService;
+  let repository: MockType<Repository<Wallet>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WalletService],
+      providers: [
+        WalletService,
+        {
+          provide: getRepositoryToken(Wallet),
+          useFactory: mockWalletRepositoryFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<WalletService>(WalletService);
+    repository = module.get(getRepositoryToken(Wallet));
   });
 
   it('should be defined', () => {
