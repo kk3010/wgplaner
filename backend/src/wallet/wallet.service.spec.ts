ca5106a6b3e5035342ee5e5826bdd0f48b4b9cdd
Chris <ch148@hdm-stuttgart.de>
2021-12-24 09:54:06 +0100

    wallet service tests

--- backend/src/wallet/wallet.service.spec.ts
+++ backend/src/wallet/wallet.service.spec.ts
@@ -5,10 +5,15 @@ import { Wallet } from './entities/wallet.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { generateFakeWallet } from '../../test/wallet/wallet.mock';
 import { generateFakeUser } from '../../test/user/user.mock';
+import { generateFakeFlat } from '../../test/flat/flat.mock';
 import type { MockType } from '../../test/mockType';
-import type { IUser } from '../interfaces/user.interface';
+import { IWallet } from 'src/interfaces/wallet.interface';
 
 const mockWalletRepositoryFactory: () => MockType<Repository<Wallet>> = () => ({
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
   update: jest.fn(),
 });
 
@@ -33,6 +38,76 @@ describe('WalletService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new wallet', async () => {
+      const user = generateFakeUser(1);
+
+      const expected: IWallet = {
+        ...generateFakeWallet(user),
+        balance: 0,
+      };
+
+      jest.spyOn(repository, 'create').mockResolvedValue(expected);
+      jest.spyOn(repository, 'save').mockResolvedValue(expected);
+
+      expect(await service.create(user)).toEqual(expected);
+      expect(repository.create).toHaveBeenCalled();
+      expect(repository.save).toHaveBeenCalled();
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all wallets of a flat', async () => {
+      const flat = generateFakeFlat();
+      const user1 = generateFakeUser(flat.id);
+      const user2 = generateFakeUser(flat.id);
+      const user3 = generateFakeUser(flat.id);
+
+      const expected: IWallet[] = [
+        generateFakeWallet(user1),
+        generateFakeWallet(user2),
+        generateFakeWallet(user3),
+      ];
+
+      jest.spyOn(repository, 'find').mockResolvedValue(expected);
+
+      expect(await service.findAll(flat.id)).toEqual(expected);
+      expect(repository.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOneById', () => {
+    it('should return wallet with matching id', async () => {
+      const flat = generateFakeFlat();
+      const user = generateFakeUser(flat.id);
+
+      const expected: IWallet = {
+        ...generateFakeWallet(user),
+      };
+
+      jest.spyOn(repository, 'findOne').mockResolvedValue(expected);
+
+      expect(await service.findOneById(expected.id)).toEqual(expected);
+      expect(repository.findOne).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOneByUserId', () => {
+    it('should return wallet owned by matching user', async () => {
+      const flat = generateFakeFlat();
+      const user = generateFakeUser(flat.id);
+
+      const expected: IWallet = {
+        ...generateFakeWallet(user),
+      };
+
+      jest.spyOn(repository, 'findOne').mockResolvedValue(expected);
+
+      expect(await service.findOneByUserId(user.id, flat.id)).toEqual(expected);
+      expect(repository.findOne).toHaveBeenCalled();
+    });
   });
 
   describe('updateBalance', () => {
