f0d2943ee368f8541ab04f5ba0189665a803a874
Ennen Chris-Robin <ce053@hdm-stuttgart.de>
2022-01-16 20:29:33 +0100

    fix dev config proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-function getBackendUrl(env) {
-  return env === 'development' ? 'localhost:5000' : 'backend:5000'
-}
-
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
   resolve: {
     alias: {
@@ -18,10 +14,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://' + getBackendUrl(process.env.NODE_ENV),
+        target: 'http://' + mode === 'development' ? 'localhost:5000' : 'backend:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-})
+}))
