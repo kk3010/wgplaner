3cc4f35c7d3673403d09ef962b5086d3b3cc639e
Karyna Kachanovska <kk153@hdm-stuttgart.de>
2022-01-17 18:02:25 +0100

    fix vite config by assigning backend url to a variable rather than defining it inline don't ask

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,21 +3,24 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@interfaces': resolve(__dirname, '../common/interfaces'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://' + mode === 'development' ? 'localhost:5000' : 'backend:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const backendUrl = mode === 'development' ? 'localhost:5000' : 'backend:5000'
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '@interfaces': resolve(__dirname, '../common/interfaces'),
       },
     },
-  },
-}))
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://' + backendUrl,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
+})
